44489a2e55f1fa2fca9f306ac116491b4b43ef5f
nonnax <ironald@gmail.com>
2022-03-14 15:06:16 +0800

    update: head.html

--- scrape.rb
+++ scrape.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 # Id$ nonnax 2022-03-11 20:08:41 +0800
-ARGF.read.scan(/\[([^\]]+?)\]/m).each do |section|
+ARGF.read.scan(/\{([^\}]+)\}/m).each do |section|
   puts section
   puts
 end
